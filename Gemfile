7ae1984e1e2200c79db2b678e5792d77410bef5d
pdkproitf <phucdaica.pro@gmail.com>
2016-12-06 11:05:52 +0700

    add subdomain api and test

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'active_model_serializers'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
  namespace  :api, defaults: { format: :json },
-                  constraints: { subdomain: 'api' },
-                  path: '/' do
-  # We are going to list our resource
+                  constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+                  constraints: ApiConstraints.new(version: 1, default: true) do
+    end
  end
 end
