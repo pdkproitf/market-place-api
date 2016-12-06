3dbe77ab205d2c0028b3e404076c856341fb4e33
pdkproitf <phucdaica.pro@gmail.com>
2016-12-06 09:12:59 +0700

    Set the routes contraints for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 MarketPlaceApi::Application.routes.draw do
- namespace  :api do
+ namespace  :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' },
+                  path: '/' do
   # We are going to list our resource
  end
 end
