21fbc475b73e31be16c6a15289d762e4cce4e1ad
pdkproitf <phucdaica.pro@gmail.com>
2016-12-07 16:35:46 +0700

    Adds sessions controller create action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
   # => include test request
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
