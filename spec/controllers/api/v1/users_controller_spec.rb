a580e3c2efa1070c8b7b958eb0437666e29266b0
pdkproitf <phucdaica.pro@gmail.com>
2016-12-07 15:48:27 +0700

    Refactors test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
-
   describe "GET #show"  do
     before(:each) do
       @user = FactoryGirl.create :user
