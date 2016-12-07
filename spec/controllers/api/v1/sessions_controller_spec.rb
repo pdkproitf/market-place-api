5d06e1a92d9adaf1aa3a72d90d2c4d8c3352cad8
pdkproitf <phucdaica.pro@gmail.com>
2016-12-07 16:46:32 +0700

    Adds authenticable module for managing authentication methods

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -35,5 +35,17 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
 
       it { should respond_with 422 }
     end
- end
+  end
+
+  describe "DELETE #destroy" do
+
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
+
+  end
 end
