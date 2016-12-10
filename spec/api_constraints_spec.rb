d2ca6084ee94537426c368436ad68b88956ce614
pdkproitf <phucdaica.pro@gmail.com>
2016-12-10 16:41:26 +0700

    Allows the order to be placed along with product quantity

--- spec/api_constraints_spec.rb
+++ spec/api_constraints_spec.rb
@@ -7,14 +7,14 @@ describe ApiConstraints do
   describe "matches?" do
 
     it "returns true when the version matches the 'Accept' header" do
-      request = double(host: 'api.marketplace.dev',
+      request = double(host: 'api.v1.localhost:3000',
                        headers: {"Accept" => "application/vnd.marketplace.v1"})
-      api_constraints_v1.matches?(request).should be_true
+      api_constraints_v1.matches?(request).should be_truthy
     end
 
     it "returns the default version when 'default' option is specified" do
-      request = double(host: 'api.marketplace.dev')
-      api_constraints_v2.matches?(request).should be_true
+      request = double(host: 'api.v1.localhost:3000')
+      api_constraints_v2.matches?(request).should be_truthy
     end
   end
 end
